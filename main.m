5c675cd5105bdd9e53d21d2fe883a015d6a7d62b
JeroenVerberneDemcon <98587984+JeroenVerberneDemcon@users.noreply.github.com>
2022-01-28 13:29:15 +0100

    Create main.m

---
+++ main.m
@@ -0,0 +1,12 @@
+function x = printGeneration(aGeneration)
+    str = "";
+    for i = 1:length(aGeneration)
+        if aGeneration(i) == 1
+            str = strcat(str, "*");
+        else
+            str = strcat(str, " ");
+        end
+    end
+    disp(str);
+    x = 0;
+end
